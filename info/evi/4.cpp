43713bbdcd2ad536872d3bf5a594fb31a248f025
Darius <77147557+SDarius08@users.noreply.github.com>
2021-04-06 13:21:55 +0300

    Create 4.cpp

---
+++ info/evi/4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int n, m;
+    cin >> n >> m;
+    int c = n + m;
+    int C[c];
+    for (int i = 0; i<c; i++) {
+        cin >> C[i];
+    }
+    for (int i = 0; i<c-1; i++) {
+        for (int j = i; j<c; j++) {
+            if (C[i] > C[j]) {
+                int aux = C[i];
+                C[i] = C[j];
+                C[j] = aux;
+            }
+        }
+    }
+    for (int i = 0; i<c; i++) {
+        cout << C[i] << ' ';
+    }
+}
